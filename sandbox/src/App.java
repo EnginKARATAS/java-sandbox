1bc8fa71904bac82daa1f5387e5011008860cd8c
Engin Karatas <enginkaratas99@gmail.com>
2025-02-22 01:37:52 +0300

    add scanner int float

--- sandbox/src/App.java
+++ sandbox/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int sayi1, sayi2, toplam;
+        float ortalama;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("1. Sayı : ");
+        sayi1 = scan.nextInt();
+        sayi2 = scan.nextInt();
+        toplam = sayi1 + sayi2;
+        ortalama = (float) toplam / 2;
+        System.out.println("toplam: " + toplam);
+        System.out.println("ortalama: " + ortalama);
     }
 }
